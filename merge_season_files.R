498a850428f5eab8247b90cdfb943a66a0ea4e41
neilcharles <neil.d.charles@gmail.com>
2016-02-17 21:55:00 +0000

    stop merged file being reloaded if run twice

--- merge_season_files.R
+++ merge_season_files.R
@@ -5,19 +5,23 @@ file.dir <- "C:/path to downloaded files/"
 filelist <- list.files(file.dir)
 
 for (i in 1:length(filelist)){
-    imported.original <- read.csv(paste(file.dir,filelist[i],sep=""), stringsAsFactors = FALSE)
-
-    imported.long <- melt(imported.original,id.vars = c("Div","Date","HomeTeam","AwayTeam"))
-    
-    imported.long$Date <- as.Date(imported.long$Date,"%d/%m/%y")
-
-    if (i==1) {
-      merged.df <- imported.long      
-    } else {
-      merged.df <- rbind(merged.df,imported.long)
-  }
+  
+    if (filelist[i]!="merged.csv"){
+      
+      imported.original <- read.csv(paste(file.dir,filelist[i],sep=""), stringsAsFactors = FALSE)
+  
+      imported.long <- melt(imported.original,id.vars = c("Div","Date","HomeTeam","AwayTeam"))
+      
+      imported.long$Date <- as.Date(imported.long$Date,"%d/%m/%y")
+  
+      if (i==1) {
+        merged.df <- imported.long      
+      } else {
+        merged.df <- rbind(merged.df,imported.long)
+      }
+    }
 }
 
-imported.wide <- dcast(merged.df, Div + Date + HomeTeam + AwayTeam ~ variable)
+merged.wide <- dcast(merged.df, Div + Date + HomeTeam + AwayTeam ~ variable)
 
-write.csv(imported.wide,paste(file.dir,"merged.csv",sep=""), row.names = FALSE)
+write.csv(merged.wide,paste(file.dir,"merged.csv",sep=""), row.names = FALSE)
